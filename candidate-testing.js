e304552a24f0fb6fdb4fe99da8fde3de9bc757dc
Billie-LS <lskylizard@yahoo.com>
2024-01-13 07:04:58 -0600

    adding arrays

--- candidate-testing.js
+++ candidate-testing.js
@@ -43,6 +43,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   // console.log(question);
   // candidateAnswer = input.question(question);  code for part 1
+  
   // iterate questions array 
   for (let i = 0; i < questions.length; i++) {
     // reassign candidateAnswers to array with each iterations
